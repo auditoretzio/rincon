aa56981747b70a03083796458cb42eb3a009a583
Emmagz <cgomez@ospia.org.ar>
2026-01-15 11:43:22 -0300

    Fix database persistence on Render and Cloudinary link stability

--- rincon_pescador/settings.py
+++ rincon_pescador/settings.py
@@ -98,16 +98,13 @@ WSGI_APPLICATION = 'rincon_pescador.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# Database
+# Use DATABASE_URL from Render if available, otherwise use SQLite
 DATABASES = {
-    # 'default': dj_database_url.config(
-    #     # Feel free to alter this value to suit your needs.
-    #     default='mysql://egomez:Haiti513@localhost:3306/rincon_db',
-    #     conn_max_age=600
-    # )
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 

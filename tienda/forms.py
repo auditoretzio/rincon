15c2dc85fb2866cfaa45e77201237390191d851c
auditoretzio <emmagz3091@gmail.com>
2026-01-16 15:24:37 -0300

    Enhance forms with custom widgets and attributes
    Added custom widgets to OrdenForm fields and updated CantidadForm.

--- tienda/forms.py
+++ tienda/forms.py
@@ -5,6 +5,11 @@ class OrdenForm(forms.ModelForm):
     class Meta:
         model = Orden
         fields = ['nombre', 'email', 'telefono']
+        widgets = {
+            'nombre': forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' '}),
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': ' '}),
+            'telefono': forms.TextInput(attrs={'class': 'form-control', 'placeholder': ' '}),
+        }
 
 class CantidadForm(forms.Form):
-    cantidad = forms.IntegerField(min_value=1, initial=1)
+    cantidad = forms.IntegerField(min_value=1, initial=1, widget=forms.NumberInput(attrs={'class': 'form-control'}))
